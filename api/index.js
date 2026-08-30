c0606380dc4be4f63dab88b91aab8b19f322d259
Sadia Binte Kamal <sadiabintekamal.02@gmail.com>
2026-08-31 01:34:11 +0600

    Zero dependency api/index.js test

--- api/index.js
+++ api/index.js
@@ -1,26 +1,7 @@
-const mongoose = require('mongoose');
-
-const fallbackMongoURI = 'mongodb+srv://fixnestAdmin:123fixnest@cluster0.q7gvbmz.mongodb.net/fixnest?appName=Cluster0';
-
-const providerSchema = new mongoose.Schema({}, { strict: false });
-const Provider = mongoose.models.Provider || mongoose.model('Provider', providerSchema, 'providers');
-
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(204).end();
-  }
-
-  try {
-    if (mongoose.connection.readyState !== 1) {
-      await mongoose.connect(fallbackMongoURI, { serverSelectionTimeoutMS: 5000 });
-    }
-    const providers = await Provider.find({ verificationStatus: 'verified' });
-    res.status(200).json(providers);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  res.status(200).json([
+    { _id: "1", name: "Rahim Plumbing", serviceType: "Plumbing", rating: 4.8, address: "Mirpur, Dhaka", pricePerHour: 500 },
+    { _id: "2", name: "AquaFix Plumbing", serviceType: "Plumbing", rating: 4.6, address: "Gulshan, Dhaka", pricePerHour: 450 }
+  ]);
 };
