e8fb4160ba4df47b99ef0ddbd4370399607cd65a
Sadia Binte Kamal <sadiabintekamal.02@gmail.com>
2026-08-31 01:42:36 +0600

    Minimal Express test app in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,24 @@
-const app = require('../server/index.js');
+const express = require('express');
+const cors = require('cors');
 
-module.exports = (req, res) => {
-  return app(req, res);
-};
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/providers', (req, res) => {
+  res.json([
+    {
+      _id: "1",
+      name: "Rahim Plumbing Services",
+      serviceType: "Plumbing",
+      address: "Mirpur-10, Dhaka",
+      rating: 4.7,
+      reviewCount: 12,
+      pricePerHour: 500,
+      location: { type: "Point", coordinates: [90.4125, 23.8103] },
+      availability: [{ day: "Saturday", slots: ["09:00", "11:00"] }]
+    }
+  ]);
+});
+
+module.exports = app;
