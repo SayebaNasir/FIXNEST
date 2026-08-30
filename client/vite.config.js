67d99fe4224096bfd8e5d3049a911f95077464b7
Sadia Binte Kamal <sadiabintekamal.02@gmail.com>
2026-08-31 01:57:10 +0600

    Rename JS output bundle to fixnest-[hash].js and add no-cache meta tags to obliterate browser cache

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/fixnest-[hash].js`,
+        chunkFileNames: `assets/fixnest-[hash].js`,
+        assetFileNames: `assets/fixnest-[hash].[ext]`
+      }
+    }
+  }
 })
