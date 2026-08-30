51cf1bec9a12c934becd33f01508a7f7c18f87fb
Sadia Binte Kamal <sadiabintekamal.02@gmail.com>
2026-08-31 01:33:19 +0600

    Native Vercel serverless function test for api/index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,26 @@
-const app = require('../server/index.js');
+const mongoose = require('mongoose');
 
-module.exports = (req, res) => {
-  return app(req, res);
+const fallbackMongoURI = 'mongodb+srv://fixnestAdmin:123fixnest@cluster0.q7gvbmz.mongodb.net/fixnest?appName=Cluster0';
+
+const providerSchema = new mongoose.Schema({}, { strict: false });
+const Provider = mongoose.models.Provider || mongoose.model('Provider', providerSchema, 'providers');
+
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
+  try {
+    if (mongoose.connection.readyState !== 1) {
+      await mongoose.connect(fallbackMongoURI, { serverSelectionTimeoutMS: 5000 });
+    }
+    const providers = await Provider.find({ verificationStatus: 'verified' });
+    res.status(200).json(providers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
