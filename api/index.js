c0af087d231532e8efc53dd4ca68edb7668bb49a
Sadia Binte Kamal <sadiabintekamal.02@gmail.com>
2026-08-31 01:06:09 +0600

    Standalone express test in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
-const app = require('../server/index.js');
+const express = require('express');
+const app = express();
+
+app.get('/api/providers', (req, res) => {
+  res.json({ message: "Express serverless working on Vercel!" });
+});
 
 module.exports = app;
