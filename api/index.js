537eef51ec5e52b59532f570105a4c1bed17fdc8
Sadia Binte Kamal <sadiabintekamal.02@gmail.com>
2026-08-31 01:37:29 +0600

    Report initError in api/index.js for Vercel debugging

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
-const app = require('../server/index.js');
+let app;
+let initError = null;
+
+try {
+  app = require('../server/index.js');
+} catch (err) {
+  initError = err;
+}
 
 module.exports = (req, res) => {
+  if (initError) {
+    return res.status(500).json({
+      error: 'Serverless Function Init Failed',
+      message: initError.message,
+      stack: initError.stack
+    });
+  }
   return app(req, res);
 };
