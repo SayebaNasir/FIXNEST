169a065fe6257535bbdcf467f441ef7c24857390
Sadia Binte Kamal <sadiabintekamal.02@gmail.com>
2026-08-31 01:43:43 +0600

    Explicit @vercel/node and @vercel/static-build configuration

--- api/index.js
+++ api/index.js
@@ -1,24 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/providers', (req, res) => {
-  res.json([
-    {
-      _id: "1",
-      name: "Rahim Plumbing Services",
-      serviceType: "Plumbing",
-      address: "Mirpur-10, Dhaka",
-      rating: 4.7,
-      reviewCount: 12,
-      pricePerHour: 500,
-      location: { type: "Point", coordinates: [90.4125, 23.8103] },
-      availability: [{ day: "Saturday", slots: ["09:00", "11:00"] }]
-    }
-  ]);
-});
+const app = require('../server/index.js');
 
 module.exports = app;
