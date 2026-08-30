212f0b13bfd62228b6d6c422129c39063e2719b4
Sadia Binte Kamal <sadiabintekamal.02@gmail.com>
2026-08-31 01:36:03 +0600

    Remove conflicting api files and set single Express serverless entrypoint

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
+const app = require('../server/index.js');
+
 module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json([
-    { _id: "1", name: "Rahim Plumbing", serviceType: "Plumbing", rating: 4.8, address: "Mirpur, Dhaka", pricePerHour: 500 },
-    { _id: "2", name: "AquaFix Plumbing", serviceType: "Plumbing", rating: 4.6, address: "Gulshan, Dhaka", pricePerHour: 450 }
-  ]);
+  return app(req, res);
 };
--- api/providers.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify([
-    { _id: "1", name: "Test Provider", serviceType: "Plumbing", rating: 4.8, address: "Dhaka" }
-  ]));
-};
